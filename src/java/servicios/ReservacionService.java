868175ae8b8399e37f5a8c4865a39d052ee3912c
chesscon <chesscon@gmail.com>
2014-06-10 13:13:45 -0500

    reservacioes

--- src/java/servicios/ReservacionService.java
+++ src/java/servicios/ReservacionService.java
@@ -1,6 +1,7 @@
 
 package servicios;
 
+import dominio.Escuela;
 import dominio.Reservacion;
 import modelo.DAO;
 
@@ -12,6 +13,10 @@ public class ReservacionService {
 
     public void guardar(Reservacion reservacion) {
         DAO.insert("reservacion", "tipo, fecha", reservacion.getId() + ", " + reservacion.getFecha());
+    }
+
+    public void guardarEscuela(Escuela escuela) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
